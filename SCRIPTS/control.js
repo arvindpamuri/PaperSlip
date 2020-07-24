080131adcb4e1238fca16fbcdceebbb39cac73f3
arvind pamuri <aravind.pamuri@gmail.com>
2020-07-24 15:19:54 +0530

    adding bootstrap for styling

--- SCRIPTS/control.js
+++ SCRIPTS/control.js
@@ -1,11 +1,11 @@
 
 //initialize arrays for tables
-let itemList = [];
+let todoList = [];
 let doneList = [];
 
 //select the tables
 const todo_table = document.querySelector('#todotable');
-const done_table = document.querySelector('donetable');
+const done_table = document.querySelector('#donetable');
 
 //function called when add button is clicked
 function new_item()
@@ -20,38 +20,52 @@ function new_item()
     //add values to objects
     item.name = inp1.value;
     item.qty = inp2.value;
+    item.time = new Date().valueOf();
 
     //add obj to array
-    itemList.push(item);
+    todoList.push(item);
 
     //display the array of objects
-    display_table(itemList);
+    display_table(todo_table, todoList);
 }
 
 //display the table on screen
-function display_table(lst)
+function display_table(table, lst)
 {   
     //map html to each item of array
     const all_items = lst.map((item) => display_item(item));
     
     const joined_all_items = all_items.join(``);
 
-    console.log(all_items);
-    console.log(joined_all_items);
     //Add to display table
-    todo_table.innerHTML = joined_all_items;
+    table.innerHTML = joined_all_items;
+    console.log(lst);
 }
 
-function display_item({ name, qty })
+function item_done(seconds)
+{   
+    let index = -1;
+    for(let i=0; i<todoList.length; i++)
+    {   
+        if (todoList[i].time == seconds)
+            doneList.push(todoList[i]);
+            todoList.splice(i,1);
+    }
+    display_table(done_table, doneList);
+    display_table(todo_table, todoList);
+}
+
+function display_item({ name, qty, time })
 {   
     return `
-        <tr>
-            <td style="height:50px;width:100px">${name}</td>
-            <td style="height:5px;width:50px">${qty}</td>
-            <td style="height:50px;width:50px">
-                <button type="button" style='border: 0; padding: 0;'><i class="fas fa-check-circle fa-2x" style="border: none"></i></button>
-            </td>
-        
-        </tr>
+        <li class="list-group-item list-group-item-light d-flex justify-content-between">
+            <div class='d-flex flex-column'>${name}</div>
+            <div>
+                <span>${qty}</span>
+                <span>
+                <button type="button" class="btn btn-outline-light" onClick="item_done(${time.valueOf()})"><i class="fas fa-check-circle fa-2x" style="border: none"></i></button>
+                </span>
+            </div
+        </li>
         `;
 }
