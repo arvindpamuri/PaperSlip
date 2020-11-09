0059b73b0f033156ef184593982bfa37129e0d56
arvind pamuri <aravind.pamuri@gmail.com>
2020-11-09 21:40:29 +0530

    UI Enhancement

--- lists.js
+++ lists.js
@@ -85,26 +85,24 @@ function display_table(lst, table)
 function display_todo_item({ name, qty, time })
 {   
     return `
-        <li class="list-group-item list-group-item-secondary d-flex justify-content-between">
-            <div class='d-flex flex-column'>${name}</div>
-            <div>
-                ${qty}
-            </div>
-                <button type="button" class="btn btn-success" onClick="item_done(${time})"><i class="fas fa-check-circle fa-1x" style="border: none"></i></button>
+        <li class="list-group-item list-group-item-secondary justify-content-between mb-0 mr-0">
+        <div class="container">
+            <div class="row">
+                <span class="col-6 flex-column">${name}</span>
+                <h4 class="col-3"><span class="badge badge-pill badge-light">${qty}</span></h4>
+                <div class="col-3"><button type="button" class="btn btn-success" onClick="item_done(${time})"><i class="fas fa-check-circle fa-1x" style="border: none"></i></button></div>
             </div
         </li>
         `;
 }
-
+S
 function display_done_item({ name, qty, time })
 {   
     return `
         <li class="list-group-item list-group-item-secondary d-flex justify-content-between">
-            <div class='d-flex flex-column'>${name}</div>
-            <div>
-                ${qty}
-            </div>
-                <button type="button" class="btn btn-danger" onClick="delete_item(${time})"><i class="fas fa-trash fa-1x" style="border: none"></i></button>
+        <h5><div class='d-flex flex-column'>${name}</div><h5>
+        <h4><span class="badge badge-pill badge-light">${qty}</span></h4>
+            <button type="button" class="btn btn-danger" onClick="delete_item(${time})"><i class="fas fa-trash fa-1x" style="border: none"></i></button>
             </div
         </li>
         `;
@@ -128,3 +126,4 @@ function clearList(lst)
 
     
 }
+
